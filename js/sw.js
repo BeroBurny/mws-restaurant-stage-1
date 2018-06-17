e469920f500819da44d61cb4c3ed2f9e536e84c7
WeedDominator <bero4net@gmail.com>
2018-06-17 22:33:03 +0200

    cache..

---
+++ js/sw.js
@@ -0,0 +1,19 @@
+const CACHE_VERSION = 1;
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(`restaurant-v${CACHE_VERSION}`)
+      .then(cache => cache.addAll([
+        '/restaurant.html',
+        '/index.html',
+        'css/styles.css',
+        'js/main.js',
+        'js/dbhelper.js',
+        'js/restaurant_info.js',
+        'img/',
+        'data/restaurants.json',
+      ])).catch(function(error){
+      console.log("Failed to open Cache" + error);
+    })
+  );
+});
